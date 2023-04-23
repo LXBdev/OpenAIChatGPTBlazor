40bc08864871c9fe4831bc31988e8147062bab1b
Alex <xy.hochdorf@gmx.de>
2023-04-23 22:21:22 +0200

    Switch to Azure.AI.OpenAI

--- OpenAIChatGPTBlazor/Program.cs
+++ OpenAIChatGPTBlazor/Program.cs
@@ -1,4 +1,5 @@
-using OpenAI_API;
+using Azure;
+using Azure.AI.OpenAI;
 using OpenAIChatGPTBlazor.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,14 +8,15 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
-builder.Services.AddScoped<OpenAIAPI>(sp =>
+builder.Services.AddScoped<OpenAIClient>(sp =>
 {
     var configuration = sp.GetRequiredService<IConfiguration>();
     var apiKey = configuration["OpenAI:ApiKey"];
     var deploymentId = configuration["OpenAI:DeploymentId"];
     var resourceName = configuration["OpenAI:ResourceName"];
-    var client = OpenAIAPI.ForAzure(resourceName, deploymentId, apiKey);
-    client.ApiVersion = "2023-03-15-preview";
+    var client = new OpenAIClient(
+        new Uri($"https://{resourceName}.openai.azure.com/"),
+        new AzureKeyCredential(apiKey));
     return client;
 });
 
