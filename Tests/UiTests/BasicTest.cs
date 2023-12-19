8b03fb154c9ca31b32154ae2ee3d265ce242f3ab
Alexander <noneofyourbusiness@example.com>
2023-12-19 23:18:28 +0100

    additional test

---
+++ Tests/UiTests/BasicTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace UiTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class BasicTest : PageTest
+{
+    [SetUp]
+    public async Task SetUp()
+    {
+        // Navigate to your application hosted locally or remotely
+        await Page.GotoAsync("http://localhost:5255"); // Replace with your actual application URL
+    }
+
+    [Test]
+    public async Task NextAreaShouldHaveFocusAfterLoading()
+    {
+        await Expect(Page.GetByText("You are the assistant of a software engineer" )).ToBeVisibleAsync();
+    }
+}
