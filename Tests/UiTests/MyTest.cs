1fb31645095936b2cf09f6a745c40595d5a96ee3
Alexander <noneofyourbusiness@example.com>
2023-12-19 13:37:55 +0100

    Add Ui Test project

---
+++ Tests/UiTests/MyTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace UiTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class MyTest : PageTest
+{
+    [Test]
+    public async Task ShouldHaveTheCorrectSlogan()
+    {
+        await Page.GotoAsync("https://playwright.dev");
+        await Expect(Page.Locator("text=enables reliable end-to-end testing for modern web apps")).ToBeVisibleAsync();
+    }
+
+    [Test]
+    public async Task ShouldHaveTheCorrectTitle()
+    {
+        await Page.GotoAsync("https://playwright.dev");
+        var title = Page.Locator(".navbar__inner .navbar__title");
+        await Expect(title).ToHaveTextAsync("Playwright");
+    }
+}
