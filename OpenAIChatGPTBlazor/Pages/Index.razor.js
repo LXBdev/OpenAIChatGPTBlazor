3d91a7029151900f6eea75738fa4d64e09bb8543
lxbdev <noneofyourbusiness@example.com>
2024-03-29 16:15:14 +0100

    colocate JS instead of using host.cshtml

---
+++ OpenAIChatGPTBlazor/Pages/Index.razor.js
@@ -0,0 +1,15 @@
+function scrollElementToEnd (element) {
+    element.scrollTop = element.scrollHeight;
+}
+
+async function downloadFileFromStream (fileName, contentStreamReference) {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+    URL.revokeObjectURL(url);
+}
