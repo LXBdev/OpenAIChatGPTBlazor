d63bf32d3be05e954b43d9ea1604c82ed9d21a62
lxbdev <noneofyourbusiness@example.com>
2024-03-30 14:28:30 +0100

    migrate from .net6 to .net7

--- OpenAIChatGPTBlazor/Program.cs
+++ OpenAIChatGPTBlazor/Program.cs
@@ -6,26 +6,23 @@ using Microsoft.Extensions.Options;
 using Microsoft.FeatureManagement;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.ConfigureAppConfiguration(options =>
+var configuration = builder.Configuration;
+var opt = configuration.GetSection("AppConfig").Get<AppConfigOptions>();
+if (opt != null && !string.IsNullOrEmpty(opt.Endpoint))
 {
-    var settings = options.Build();
-    var opt = settings.GetSection("AppConfig").Get<AppConfigOptions>();
-    if (opt != null && !string.IsNullOrEmpty(opt.Endpoint))
+    configuration.AddAzureAppConfiguration(o =>
     {
-        options.AddAzureAppConfiguration(o =>
-        {
-            o.Connect(new Uri(opt.Endpoint), new DefaultAzureCredential());
+        o.Connect(new Uri(opt.Endpoint), new DefaultAzureCredential());
 
-            o.Select("*");
-            o.Select("*", opt.Label);
-            o.UseFeatureFlags(of =>
-            {
-                of.Select("*");
-                of.Select("*", opt.Label);
-            });
+        o.Select("*");
+        o.Select("*", opt.Label);
+        o.UseFeatureFlags(of =>
+        {
+            of.Select("*");
+            of.Select("*", opt.Label);
         });
-    }
-});
+    });
+}
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor()
