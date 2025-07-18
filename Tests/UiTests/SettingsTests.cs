16148e04df57f1a39a2ab55bd5b6f258c13fb110
lxbdev <noneofyourbusiness@example.com>
2025-07-18 18:41:43 +0200

    fix(settings-tests): Improved localStorage check in LocalStorageShouldHaveUpdatedValueAfterChanges test

--- Tests/UiTests/SettingsTests.cs
+++ Tests/UiTests/SettingsTests.cs
@@ -30,8 +30,9 @@ public class SettingsTests : PageTest
 
         // When
         await Page.ClickAsync("#isAutoscrollEnabled");
+
         await Page.WaitForFunctionAsync(
-            $"() => localStorage.getItem('IsAutoscrollEnabled') !== '{checkedBefore}'"
+            "() => localStorage.getItem('IsAutoscrollEnabled') === 'false'"
         );
 
         // Then
